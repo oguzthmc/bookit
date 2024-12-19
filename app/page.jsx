4a7d919750d505291d329f8a771775ff7f1b9492
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2024-12-19 12:27:19 +0300

    homepage rooms (json data)

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,13 @@
+import rooms from "@/data/rooms.json";
+
 export default function Home() {
   return (
     <>
-      <h1>Bookit App</h1>
+      {rooms.length > 0 ? (
+        rooms.map((room) => <h3>{room.name}</h3>)
+      ) : (
+        <p>No rooms available at the moment</p>
+      )}
     </>
   );
 }
