f33acbdaea486cf44cd2f85b5b6766da36c1576b
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2024-12-31 17:28:08 +0300

    get all rooms

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,15 @@
-import Heading from "@/components/Heading";
 import RoomCard from "@/components/RoomCard";
-import rooms from "@/data/rooms.json";
+import Heading from "@/components/Heading";
+import getAllRooms from "./actions/getAllRooms";
 
-export default function Home() {
+export default async function Home() {
+  const rooms = await getAllRooms();
+
   return (
     <>
       <Heading title="Available Rooms" />
       {rooms.length > 0 ? (
-        rooms.map((room) => {
-          return <RoomCard room={room} />;
-        })
+        rooms.map((room) => <RoomCard room={room} key={room.$id} />)
       ) : (
         <p>No rooms available at the moment</p>
       )}
