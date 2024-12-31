13428d969a335fe9de677cb85ba314ec142d69bb
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2024-12-31 23:27:56 +0300

    login & register pages/forms

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-import Link from "next/link";
-import logo from "@/assets/images/logo.svg";
-import { FaUser, FaSignInAlt, FaSignOutAlt, FaBuilding } from "react-icons/fa";
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '@/assets/images/logo.svg';
+import { FaUser, FaSignInAlt, FaSignOutAlt, FaBuilding } from 'react-icons/fa';
 
 const Header = () => {
   return (
@@ -27,13 +27,13 @@ const Header = () => {
                 </Link>
                 {/* <!-- Logged In Only --> */}
                 <Link
-                  href="/bookings.html"
+                  href="/bookings"
                   className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
                 >
                   Bookings
                 </Link>
                 <Link
-                  href="/add-room.html"
+                  href="/room/add"
                   className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
                 >
                   Add Room
@@ -46,24 +46,24 @@ const Header = () => {
             <div className="ml-4 flex items-center md:ml-6">
               {/* <!-- Logged Out Only --> */}
               <Link
-                href="login.html"
+                href="/login"
                 className="mr-3 text-gray-800 hover:text-gray-600"
               >
                 <FaSignInAlt className="inline mr-1" /> Login
               </Link>
               <Link
-                href="register.html"
+                href="/register"
                 className="mr-3 text-gray-800 hover:text-gray-600"
               >
                 <FaUser className="inline mr-1" />
                 Register
               </Link>
-              <Link href="my-rooms.html">
+              <Link href="/rooms/my">
                 <FaBuilding className="inline mr-1" />
                 My Rooms
               </Link>
               <Link
-                href="login.html"
+                href="/login"
                 className="mx-3 text-gray-800 hover:text-gray-600"
               >
                 <FaSignOutAlt className="inline mr-1" /> Sign Out
@@ -84,13 +84,13 @@ const Header = () => {
           </Link>
           {/* <!-- Logged In Only --> */}
           <Link
-            href="/bookings.html"
+            href="/bookings"
             className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
           >
             Bookings
           </Link>
           <Link
-            href="/add-room.html"
+            href="/rooms/add"
             className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
           >
             Add Room
